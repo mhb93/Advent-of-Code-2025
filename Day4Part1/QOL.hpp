f991bccdc65ff1cb866cb719d5719f7725e5bea5
Mary <mhb93@nau.edu>
2025-12-11 13:55:56 -0900

    It's horribly ugly. But it gets the job done.

---
+++ Day4Part1/QOL.hpp
@@ -0,0 +1,17 @@
+#ifndef QOL_H
+#define QOL_H
+
+
+#include <fstream>  // For reading from a file
+#include <string>   // For string manipulation
+#include <vector>   // For vectors
+#include <cstdint>  // For big numbers
+#include <iostream> // For debugging and printing out the answer
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+
+#endif
---
+++ Day4Part1/compile.bat
@@ -0,0 +1,3 @@
+g++ -std=c++17 -c day4.cpp -o day4.o
+g++ -std=c++17 day4.o -o PaperMover
+.\PaperMover
